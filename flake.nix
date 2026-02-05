47c89138515c919377c77555fcec9a2f14b8dcc1
Michael Pennington <michaeldanielpennington@gmail.com>
2026-02-04 21:51:24 -0500

    Add cronos hardware config

--- flake.nix
+++ flake.nix
@@ -12,6 +12,8 @@
     # Sops-Nix for secret management
     sops-nix.url = "github:Mic92/sops-nix";
     sops-nix.inputs.nixpkgs.follows = "nixpkgs";
+
+    colmena.url = "github:zhaofengli/colmena";
   };
 
   outputs = {
@@ -19,25 +21,43 @@
     nixpkgs,
     home-manager,
     sops-nix,
+    colmena,
     ...
   } @ inputs: let
     system = "x86_64-linux";
     lib = nixpkgs.lib;
+    baseModules = [
+      home-manager.nixosModules.home-manager
+      sops-nix.nixosModules.sops
+    ];
     # Function to reduce boilerplate for creating systems
     mkSystem = host:
       lib.nixosSystem {
         inherit system;
         specialArgs = {inherit inputs;}; # Pass inputs to modules
-        modules = [
-          ./hosts/${host}
-          home-manager.nixosModules.home-manager
-          sops-nix.nixosModules.sops
-        ];
+        modules = [./hosts/${host}] ++ baseModules;
       };
   in {
     nixosConfigurations = {
       hermes = mkSystem "hermes";
       cronos = mkSystem "cronos";
+    };
+    colmenaHive = colmena.lib.makeHive {
+      meta = {
+        nixpkgs = import nixpkgs {
+          inherit system;
+        };
+        specialArgs = {inherit inputs;};
+      };
+      cronos = {
+        deployment = {
+          targetHost = "192.168.1.10";
+          tags = ["home" "private"];
+          allowLocalDeployment = true; # Allow deploying to itself if you run colmena on cronos
+        };
+
+        imports = [./hosts/cronos] ++ baseModules;
+      };
     };
   };
 }
--- hosts/cronos/default.nix
+++ hosts/cronos/default.nix
@@ -3,11 +3,13 @@
   pkgs,
   ...
 }: {
-  imports = [../../modules/common];
+  imports = [
+    ../../modules/common
+    ./hardware-configuration.nix
+  ];
   networking.hostName = "cronos";
 
   # You will need to generate this later with 'nixos-generate-config'
-  # imports = [ ./hardware-configuration.nix ];
 
   system.stateVersion = "25.11"; # Match your installed version
 }
