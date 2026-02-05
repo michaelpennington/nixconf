b4ddbf0da7a0de0c5d6abb7a7abccb5c71a9c7b8
Michael Pennington <michaeldanielpennington@gmail.com>
2026-02-04 22:10:11 -0500

    use systemdboot

--- hosts/cronos/default.nix
+++ hosts/cronos/default.nix
@@ -9,7 +9,13 @@
   ];
   networking.hostName = "cronos";
 
-  boot.loader.systemd-boot.enable = true;
-
+  boot.loader = {
+    systemd-boot.enable = true;
+    grub = {
+      enable = false;
+      device = "/dev/nvme0n1p1";
+    };
+    efi.canTouchEfiVariables = true;
+  };
   system.stateVersion = "25.11"; # Match your installed version
 }
